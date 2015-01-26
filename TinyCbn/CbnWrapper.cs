f7b485fe59b7718142163bc596505d68f2e3cdc9
Mattias Rudén <mattias@indiesoft.org>
2015-01-26 08:26:39 +0100

    Initial commit

---
+++ TinyCbn/CbnWrapper.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using ThingM.Blink1.ColorProcessor;
+
+namespace TinyCbn
+{
+    public class CbnWrapper
+    {
+        public async Task<List<Rgb>> GetColours()
+        {
+            List<Rgb> colors = new List<Rgb>();
+            using (var stream = await new HttpClient().GetStreamAsync("http://api.colourbynumbers.org/cbn-live/getColours"))
+            {
+                using (var streamReader = new StreamReader(stream))
+                {
+                    using (var jsonReader = new JsonTextReader(streamReader))
+                    {
+                        dynamic d = new JsonSerializer().Deserialize(jsonReader);
+                        for (int i = 0; i <= 9; i++)
+                        {
+                            JProperty color = ((JObject)d.colours).Property(i.ToString());
+                            JArray arr = color.Value as JArray;
+                            Rgb c = new Rgb((ushort)arr[0], (ushort)arr[1], (ushort)arr[2]);
+                            colors.Add(c);
+                        }
+                    }
+                }
+
+                return colors;
+            }
+        }
+    }
+}
---
+++ TinyCbn/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ThingM.Blink1;
+
+namespace TinyCbn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CancellationTokenSource cts = new CancellationTokenSource();
+
+            Task t = RunAsync(cts);
+
+            Console.ReadLine();
+            cts.Cancel();
+            t.Wait();
+
+            using (Blink1 blink = new Blink1())
+            {
+                blink.Open();
+                blink.SetColor(0, 0, 0);
+                blink.Close();
+            }
+
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+        }
+
+        static async Task RunAsync(CancellationTokenSource cts)
+        {
+            try
+            {
+                CbnWrapper wrapper = new CbnWrapper();
+                while (!cts.IsCancellationRequested)
+                {
+                    var colours = await wrapper.GetColours();
+                    Console.WriteLine("New colors");
+                    using (Blink1 blink = new Blink1())
+                    {
+                        blink.Open();
+                        colours.Reverse();
+
+                        blink.Blink(2, 200, 200, colours.First());
+
+                        foreach (var color in colours)
+                        {
+                            if (cts.IsCancellationRequested)
+                                break;
+                            Console.WriteLine(string.Format("Fading to ({0},{1},{2})", color.Red, color.Green, color.Blue));
+                            blink.FadeToColor(1000, color, true);
+                            Thread.Sleep(5000);
+                        }
+                    }
+                }
+            }
+            catch { }
+        }
+    }
+}
